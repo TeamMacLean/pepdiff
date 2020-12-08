0fc63c29f7c5eb51ac10908dc892cf615e84e8aa
danmaclean <maclean.daniel@gmail.com>
2020-12-08 15:21:01 +0000

    ddro

--- R/plots.R
+++ R/plots.R
@@ -325,8 +325,13 @@ plot_heatmap <- function(l, sig = 0.05, metric = NA, log = FALSE, base = 2, col_
 
 
   if (dendro){
+
     library(ggdendro)
-    ddro <- ggdendro::ggdendrogram(hc_obj, rotate=TRUE) + ggplot2::theme(axis.text.y = ggplot2::element_blank())
+    p <- p + ggplot2::theme(axis.text.x = ggplot2::element_blank(),
+                            axis.title.x = ggplot2::element_blank())
+    ddro <- ggdendro::ggdendrogram(hc_obj, rotate=TRUE) +
+      ggplot2::theme(axis.text.y = ggplot2::element_blank(),
+                     axis.text.y = ggplot2::element_blank() )
     p <- cowplot::plot_grid(ddro, p, nrow=1)
   }
 
