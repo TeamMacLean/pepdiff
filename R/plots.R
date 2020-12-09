87265addf92acff5f578fb47affbe8ae78ac0f6e
danmaclean <maclean.daniel@gmail.com>
2020-12-09 09:09:11 +0000

    ggtree

--- R/plots.R
+++ R/plots.R
@@ -326,10 +326,10 @@ plot_heatmap <- function(l, sig = 0.05, metric = NA, log = FALSE, base = 2, col_
 
   if (dendro){
 
-    library(ggdendro)
+    library(ggtree)
     #p <- p + ggplot2::theme(#axis.text.x = ggplot2::element_blank(),
                             #axis.title.x = ggplot2::element_blank())
-    ddro <- ggdendro::ggdendrogram(hc_obj, rotate=TRUE) #+
+    ddro <- ggtree(hc_obj) #+
       #ggplot2::theme(axis.text.x = ggplot2::element_blank(),
        #              axis.text.y = ggplot2::element_blank() )
     p <- cowplot::plot_grid(ddro, p, nrow=1, align = "hv")
