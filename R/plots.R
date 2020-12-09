c12da0bf7112ebbbb5dcbc0f86d8aa20adf934c4
danmaclean <maclean.daniel@gmail.com>
2020-12-09 09:37:40 +0000

    ggtree

--- R/plots.R
+++ R/plots.R
@@ -261,7 +261,7 @@ drop_columns <- function(df, sig, metric, log, base, rows_to_keep = NULL){
 #' @return ggplot2 plot
 #' @export
 #' @importFrom rlang .data
-plot_heatmap <- function(l, sig = 0.05, metric = NA, log = FALSE, base = 2, col_order = NULL, rotate_x_labels = TRUE, only_sig_points = TRUE, option="E", direction=-1, use_y_labels=TRUE, dendro = FALSE) {
+plot_heatmap <- function(l, sig = 0.05, metric = NA, log = FALSE, base = 2, col_order = NULL, rotate_x_labels = TRUE, only_sig_points = TRUE, option="E", direction=-1, use_y_labels=TRUE, dendro = FALSE, y_labels_on_right=FALSE) {
 
   if (is.null(col_order)) {
     col_order <- names(l)
@@ -323,10 +323,11 @@ plot_heatmap <- function(l, sig = 0.05, metric = NA, log = FALSE, base = 2, col_
     p <- p +ggplot2::theme(axis.text.y = ggplot2::element_blank())
   }
 
+  if (y_labels_on_right){
+    p <- p + ggplot2::scale_y_continuous(position = "right")
+  }
 
   if (dendro){
-
-    library(ggtree)
     p <- p + ggplot2::theme(#axis.text.x = ggplot2::element_blank(),
                             axis.title.y = ggplot2::element_blank())
     ddro <- ggtree(hc_obj) #+
