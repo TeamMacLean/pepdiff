7384564ddb8f446bcea1fb3ddd1191c28eadb911
danmaclean <maclean.daniel@gmail.com>
2021-07-15 14:09:42 +0100

    meh

--- R/plots.R
+++ R/plots.R
@@ -298,11 +298,10 @@ plot_heatmap <- function(l, sig = 0.05, metric = NA, log = FALSE, base = 2, col_
     x$fold_change <- log(x$fold_change, base = base)
     name <- paste("Log", base, "Fold Change")
   }
-  x <- x %>%
+  m <- x %>%
        dplyr::mutate(gene_peptide = paste(.data$gene_id, .data$peptide, sep = " " )) %>%
-    tidybulk::impute_missing_abundance(~1, .sample=comparison, .transcript=gene_peptide, .abundance=fold_change)
-
-    m <- dplyr::select(x, gene_peptide, comparison, fold_change) %>%
+    tidybulk::impute_missing_abundance(~1, .sample=comparison, .transcript=gene_peptide, .abundance=fold_change) %>%
+    dplyr::select(gene_peptide, comparison, fold_change) %>%
     tidyr::pivot_wider(names_from = comparison, values_from = fold_change)
     rnames <- m$gene_peptide
     m <- dplyr::select(m, !gene_peptide)
@@ -310,15 +309,15 @@ plot_heatmap <- function(l, sig = 0.05, metric = NA, log = FALSE, base = 2, col_
     m <-  as.matrix(m)
     rownames(m) <- rnames
     colnames(m) <- cnames
-    p <- tidyHeatmap::heatmap(x, gene_peptide, comparison, fold_change,
-    #p <- ComplexHeatmap::Heatmap(m,
+    #tidyHeatmap::heatmap(gene_peptide, comparison, fold_change,
+    p <- ComplexHeatmap::Heatmap(m,
                          column_order = col_order,
                          row_km = row_kms,
                          column_km = col_kms,
                          #column_title = col_title,
                          #row_title = row_title,
                          name = name,
-                       palette_value =  circlize::colorRamp2(
+                       col =  circlize::colorRamp2(
                            seq(scale_min, scale_max, length.out = 11),
                            rev(RColorBrewer::brewer.pal(11, pal))
                          )
